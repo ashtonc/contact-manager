3113b05db2fa42b5743303073d9a7cb1b919a2e8
Ashton Charbonneau <acharbon@sfu.ca>
2017-11-02 18:32:23 -0700

    Added new handler functions.

--- contactmanager.go
+++ contactmanager.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 type Contact struct {
-	Name string
-	//    FirstName string
-	//    LastName string
-	//    Email string
-	//    Phone string
-	Notes []byte
+	Id        int
+	Name      string
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	Notes     []byte
 }
 
 func (c *Contact) save() error {
@@ -29,13 +31,25 @@ func loadContact(name string) (*Contact, error) {
 	return &Contact{Name: name, Notes: notes}, nil
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func viewContactHandler(w http.ResponseWriter, r *http.Request) {
 	contact := r.URL.Path[len("/contact/"):]
 	c, _ := loadContact(contact)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", c.Name, c.Notes)
 }
 
+func newContactHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
 func main() {
-	http.HandleFunc("/contact/", viewHandler)
-	http.ListenAndServe(":8000", nil)
+	PORT := "8000"
+	log.Print("Running server on port " + PORT)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/contact/", viewContactHandler)
+	http.HandleFunc("/new", newContactHandler)
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
